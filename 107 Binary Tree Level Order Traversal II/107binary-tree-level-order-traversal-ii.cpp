a465404b10ad3aa26a1167ffca79f6a103db8512
shivam <shivamjogdand2580@gmail.com>
2025-06-21 05:47:34 +0000

    Code challenge submission for question: 107 Binary Tree Level Order Traversal II

---
+++ 107 Binary Tree Level Order Traversal II/107binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+  class Solution {
+public:
+ void levelnodes(int i,int l,TreeNode *root,vector<int>&v){
+    if(root==NULL) return;
+    if(l == i)
+    {v.push_back(root->val);
+               return;} 
+       
+       levelnodes(i+1,l,root->left,v) ; 
+       levelnodes(i+1,l,root->right,v) ;
+       
+        
+ }
+
+ int level(TreeNode *root){
+    if(!root) return 0; 
+    return 1+ max(level(root->left),level(root->right));
+  
+}
+
+ void fill(TreeNode*root,vector<vector<int>>&ans){
+   for(int i=level(root);i>0;i--){
+    vector<int>v;
+    levelnodes(1,i,root,v);
+    ans.push_back(v);
+
+   }
+ }
+  
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+     vector<vector<int>>ans;
+     fill(root,ans);
+     return ans;
+       
+    }
+};
+
