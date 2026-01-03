8b273f03f6bd838d3d8c551c941e4d144ec68154
shivam <shivamjogdand2580@gmail.com>
2026-01-03 15:12:31 +0000

    Code challenge submission for question: 3794 Reverse String Prefix

---
+++ 3794 Reverse String Prefix/3794reverse-string-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+
+        int i=0;
+        int j=k-1;
+        while(i<j) {
+            
+            swap(s[i],s[j]);
+            i++;
+            j--;
+                
+                }
+        return s;    
+        
+    }
+
+ 
+};
