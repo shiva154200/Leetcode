43416ef379b0dd7beb04c5b41bd29bc1175cd707
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-14 15:49:13 +0530

    Time: 0 ms (100%), Space: 9.9 MB (47.04%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,46 +1,9 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        int x1 = rec1[0];
-        int y1 = rec1[1];
-        int x2 = rec1[2];
-        int y2 = rec1[3];
-        if ((x1 == rec2[0] && x2 == rec2[2]) &&
-            (((y1 > rec2[1]) && (y1 < rec2[3])) ||
-             ((y2 > rec2[1]) && (y2 < rec2[3])) ||
-             ((y1 == rec2[1]) && (y2 == rec2[3])))) {
-            cout << 1;
-            return true;
-        }
-
-        if ((y1 == rec2[1] && y2 == rec2[3]) &&
-            (((x1 > rec2[0]) && (x1 < rec2[2])) ||
-             ((x2 > rec2[0]) && (x2 < rec2[2])) ||
-             ((x1 == rec2[0]) && (x2 == rec2[2])))) {
-            cout << 2;
-            return true;
-        }
-
-        if (((x1 > rec2[0] && x1 < rec2[2]) ||
-             (x2 > rec2[0] && x2 < rec2[2])) &&
-            ((y1 < rec2[1] && y2 > rec2[1]) || (y1 < rec2[3] && y2 > rec2[3]) ||
-             ((y1 > rec2[1] && y1 < rec2[3]) &&
-              (y2 > rec2[1] && y2 < rec2[3])))) {
-            cout << 3;
-            return true;
-        }
-
-        if (((x1 < rec2[0] && x2 > rec2[0]) ||
-             (x1 < rec2[2] && x2 > rec2[2])) &&
-            ((y1 > rec2[1] && y1 < rec2[3]) || (y2 > rec2[1] && y2 < rec2[3]) ||
-             ((y1 < rec2[1] && y2 > rec2[1]) &&
-              (y1 < rec2[3] && y2 > rec2[3])))) {
-            cout << 4;
-            return true;
-        }
-
-        cout << 9;
-
-        return false;
+        return rec1[0] < rec2[2] &&   // rec1 left < rec2 right
+               rec1[2] > rec2[0] &&   // rec1 right > rec2 left
+               rec1[1] < rec2[3] &&   // rec1 bottom < rec2 top
+               rec1[3] > rec2[1];     // rec1 top > rec2 bottom
     }
 };
