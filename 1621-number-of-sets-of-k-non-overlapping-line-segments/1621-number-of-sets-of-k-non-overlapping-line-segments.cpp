db27bfadf9ec939f0bb1d915555a7f66e7d50cac
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-17 22:51:48 +0530

    Time: 3 ms (77.73%), Space: 7.9 MB (88.63%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -27,11 +27,9 @@ public:
         while (b) {
             if (b & 1)
                 ans = ans * a % mod;
-
             a = a * a % mod;
             b >>= 1;
         }
-
         return ans;
     }
 };
