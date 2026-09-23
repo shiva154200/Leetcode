58e33ec4931749f8c8bc0ecf5cad6faa76d3779d
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-23 14:30:51 +0530

    Time: 0 ms (100%), Space: 102.3 MB (65.24%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,25 +2,64 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        unordered_map<int,int>mp;
+       
         int sum=0;
-        mp[0]=n;
+       int ans=INT_MAX;
         for(int i=n-1;i>=0;i--){
             sum+=nums[i];
-            mp[sum]=i;
-        }
-       
-        int ans=(mp.count(x))?(n-mp[x]):INT_MAX;
-        int prefixsum=0;
-        for(int i=0;i<n;i++){
-            prefixsum+=nums[i];
-            if(prefixsum>x) break;
-            if(mp.count(x-prefixsum)&&i<mp[x-prefixsum]){
-                ans=min(ans,(i+1)+(n-mp[x-prefixsum]));
+            if(sum==x){
+                ans=(n-i);
             }
+
         }
-     if(ans!=INT_MAX) return ans;
-     return -1;
+
+        if(sum==x) return n;
+        if(sum<x) return-1;
+
+ cout<<sum;
+        int maxsubarraylength=INT_MIN;
+        int subarray_sum=0;
+        int r=sum-subarray_sum;
+        int right=0;
+       
+
+        for(int i=0;i<n;i++){
+           subarray_sum+=nums[i];
+           r=sum - subarray_sum;
         
+          if(r==x){
+            maxsubarraylength=max(maxsubarraylength,i-right+1);
+            subarray_sum-=nums[right];
+              right++;
+          }
+
+          else if(r<x) {
+            while(r<x&&right<n){
+                 subarray_sum-=nums[right];
+                 right++;
+              r=sum - subarray_sum;
+  
+            }
+            if(r==x&&right<n){
+            maxsubarraylength=max(maxsubarraylength,i-right+1);
+            subarray_sum-=nums[right];
+            right++;
+          }
+
+        
+          } 
+            
+        }
+        
+        if(maxsubarraylength!=INT_MIN) ans= min(ans,(n-maxsubarraylength));
+        if (ans!=INT_MAX) return ans;
+        return -1;
+
+
+
+        
+       
+       
+     
     }
 };
