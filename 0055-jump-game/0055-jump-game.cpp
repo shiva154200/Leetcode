a5d078ccf80189f5395aa8579efce84e901e4035
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-25 17:59:10 +0530

    Time: 0 ms (100%), Space: 52.3 MB (82.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+  //hello disty ka
+  
     bool canJump(vector<int>& nums) {
-        int farthest = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > farthest) return false;     
-            farthest = max(farthest, i + nums[i]);
+        int n=nums.size();
+        int farthest=0;  //denotes how long we can go
+        for(int i=0;i<n;i++){
+            
+            if(i>farthest) return false;
+            farthest=max(farthest,i+nums[i]);
         }
+        
         return true;
+        
+        
     }
 };
+
+
+// Haa distay
+// OKAY
+//done
+// Evdach Hota ??
+
+//wait
+// Submit karrrr
