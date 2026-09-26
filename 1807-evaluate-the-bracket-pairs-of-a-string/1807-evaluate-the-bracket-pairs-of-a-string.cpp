304ff558a85efd63eab024e32b57413384d9ab0a
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-26 13:44:25 +0530

    Time: 72 ms (78.82%), Space: 122.7 MB (66.67%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
+
         unordered_map<string, string> mp;
         for (auto& v : knowledge) {
             mp[v[0]] = v[1];
@@ -8,21 +9,25 @@ public:
         string ans = "";
         bool f = false;
         string r = "";
+
         for (char& c : s) {
             if (c == '(') {
                 f = true;
                 r = "";
-            } else if (c == ')') {
-                if (mp.count(r)) {
-                    ans += mp[r];
-                }
-                else {
-                    ans.push_back('?');
-                }
+            } 
+            else if (c == ')' && mp.count(r)) {
+                ans += mp[r];
                 f = false;
-            } else if (f) {
+            } 
+            else if (c == ')') {
+                ans.push_back('?');
+                f = false;
+
+            } 
+            else if (f) {
                 r.push_back(c);
-            } else {
+            } 
+            else {
                 ans.push_back(c);
             }
         }
